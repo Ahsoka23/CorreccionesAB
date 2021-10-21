f8de8f6dc830297db392888e7051f21bf6362f89
Ahsoka23 <89179461+Ahsoka23@users.noreply.github.com>
2021-10-20 19:22:33 -0500

    Add files via upload

---
+++ js/Launcher.js
@@ -0,0 +1,35 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        //Recuerda declarar el bodyA así como declaraste el pointB
+        this.bodyA= bodyA;
+        this.pointB = pointB;
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+
+    //Declaración attached (Debía venir declarada en la plantilla. Error de plantilla)
+    //Provoca que al momento de ser ejecutada, el bodyA vuelva al cuerpo restringido
+    attach(bodyA){
+		this.launcher.bodyA=bodyA;
+	}
+
+    display(){
+        if(this.launcher.bodyA){
+        var pointA = this.launcher.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+
+    libre(){
+       this.launcher.bodyA = null;
+    }
+    
+}
